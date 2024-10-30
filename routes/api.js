68e2574d48ff29fadd8a15142d78b66e2e13a443
masha <mariyapnmrv@gmail.com>
2024-10-30 18:36:16 -0300

    Created and tested API routes

---
+++ routes/api.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+//Get the currencies
+router.get("/currencies", async (req, res) => {
+  try {
+    const currencies = await prisma.currency.findMany();
+    res.json(currencies);
+  } catch (error) {
+    console.error("Error fetching currencies:", error);
+    res.status(500).json({ error: "Error fetching currencies" });
+  }
+});
+
+//Get exchange rate for a pair
+router.get("/exchange-rate", async (req, res) => {
+  const { base, target } = req.query;
+
+  try {
+    const exchangeRate = await prisma.exchangeRate.findFirst({
+      where: {
+        baseCurrency: { currency_code: base },
+        targetCurrency: { currency_code: target },
+      },
+    });
+
+    if (exchangeRate) {
+      res.json(exchangeRate);
+    } else {
+      res.status(404).json({ error: "Exchange rate could not be found" });
+    }
+  } catch (error) {
+    console.error("Error fetching exchange rate:", error);
+    res.status(500).json({ error: "Error fetching exchange rate" });
+  }
+});
+
+module.exports = router;
